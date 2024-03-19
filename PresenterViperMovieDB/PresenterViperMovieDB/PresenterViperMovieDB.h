8990fef44e789faf5088252e975cee4be40b864b
Pedro Mezacasa Muller <pedro.mezzacasa@gmail.com>
2024-03-19 14:48:06 -0300

    Agora tem módulos para os componentes

---
+++ PresenterViperMovieDB/PresenterViperMovieDB/PresenterViperMovieDB.h
@@ -0,0 +1,18 @@
+//
+//  PresenterViperMovieDB.h
+//  PresenterViperMovieDB
+//
+//  Created by Pedro Mezacasa Muller on 19/03/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PresenterViperMovieDB.
+FOUNDATION_EXPORT double PresenterViperMovieDBVersionNumber;
+
+//! Project version string for PresenterViperMovieDB.
+FOUNDATION_EXPORT const unsigned char PresenterViperMovieDBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PresenterViperMovieDB/PublicHeader.h>
+
+
---
+++ RouterViperMovieDB/RouterViperMovieDB/RouterViperMovieDB.h
@@ -0,0 +1,18 @@
+//
+//  RouterViperMovieDB.h
+//  RouterViperMovieDB
+//
+//  Created by Pedro Mezacasa Muller on 19/03/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RouterViperMovieDB.
+FOUNDATION_EXPORT double RouterViperMovieDBVersionNumber;
+
+//! Project version string for RouterViperMovieDB.
+FOUNDATION_EXPORT const unsigned char RouterViperMovieDBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RouterViperMovieDB/PublicHeader.h>
+
+
