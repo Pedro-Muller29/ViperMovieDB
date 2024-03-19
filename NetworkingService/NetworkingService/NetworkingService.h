96d6358a7af3e5707910a86d7670be59fedff744
Pedro Mezacasa Muller <pedro.mezzacasa@gmail.com>
2024-03-19 14:19:21 -0300

    Módulo de Servico criado

---
+++ NetworkingService/NetworkingService/NetworkingService.h
@@ -0,0 +1,18 @@
+//
+//  NetworkingService.h
+//  NetworkingService
+//
+//  Created by Pedro Mezacasa Muller on 19/03/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetworkingService.
+FOUNDATION_EXPORT double NetworkingServiceVersionNumber;
+
+//! Project version string for NetworkingService.
+FOUNDATION_EXPORT const unsigned char NetworkingServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkingService/PublicHeader.h>
+
+
