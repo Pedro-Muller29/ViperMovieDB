9b928c36e1b7283a7b8a6120592dc82287c73ed8
Pedro Mezacasa Muller <pedro.mezzacasa@gmail.com>
2024-03-19 14:14:01 -0300

    Criacao do modulo de Interactor + Model

---
+++ InteractorModelViperMovieDB/InteractorModelViperMovieDB/InteractorModelViperMovieDB.h
@@ -0,0 +1,18 @@
+//
+//  InteractorModelViperMovieDB.h
+//  InteractorModelViperMovieDB
+//
+//  Created by Pedro Mezacasa Muller on 19/03/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InteractorModelViperMovieDB.
+FOUNDATION_EXPORT double InteractorModelViperMovieDBVersionNumber;
+
+//! Project version string for InteractorModelViperMovieDB.
+FOUNDATION_EXPORT const unsigned char InteractorModelViperMovieDBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InteractorModelViperMovieDB/PublicHeader.h>
+
+
